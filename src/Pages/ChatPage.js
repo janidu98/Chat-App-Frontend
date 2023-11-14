750d1ec2f05500c4454b399ed872b83da8e33b76
janidu98 <janinduchathushan@gmail.com>
2023-11-14 10:07:05 +0530

    commit 2

--- src/Pages/ChatPage.js
+++ src/Pages/ChatPage.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 const ChatPage = () => {
+
+    useEffect(() => {
+        fetchChats();
+    }, []);
+
+    const fetchChats = async() => {
+        const data = await axios.get('/api/chat');
+        console.log(data);
+    }
+
   return (
     <div>
       Chat Page
