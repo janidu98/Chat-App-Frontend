567e53fd509e260a656e9433714c6da2e6427475
janidu98 <janinduchathushan@gmail.com>
2023-11-23 01:01:57 +0530

    created chatpage sidedrawer

--- src/components/miscellaneous/SideDrawer.js
+++ src/components/miscellaneous/SideDrawer.js
@@ -1,5 +1,7 @@
-import { Box, Tooltip } from '@chakra-ui/react'
+import { Avatar, Box, Button, Menu, MenuButton, MenuItem, MenuList, Text, Tooltip } from '@chakra-ui/react'
+import { BellIcon, ChevronDownIcon } from '@chakra-ui/icons'
 import React, { useState } from 'react'
+import { ChatState } from '../../Context/ChatProvider';
 
 const SideDrawer = () => {
 
@@ -8,12 +10,51 @@ const SideDrawer = () => {
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState();
 
-  return (
-    <>
-        <Box>
-            <Tooltip label='Search Users to chat' hasArrow placement='bottom-end'>
+  const { user } = ChatState();
 
+  return (  
+    <>
+        <Box
+            display='flex' 
+            justifyContent='space-between' 
+            alignItems='center' 
+            bg='white' 
+            w='100%' 
+            p='5px 10px 5px 10px' 
+            borderWidth='5px'
+        >
+            {/* Search user */}
+            <Tooltip  label='Search Users to chat' hasArrow placement='bottom-end'>
+                <Button variant='ghost'>
+                    <i class="fas fa-search"></i>
+                    <Text display={{base: "none", md: "flex"}} px='4'>Search User</Text>
+                </Button>
             </Tooltip>
+
+            {/* Name of the app */}
+            <Text fontSize='2xl' fontFamily='Work sans'>
+                Let's Chat
+            </Text>
+
+            <div>
+                <Menu>
+                    <MenuButton p={1}>
+                        <BellIcon fontSize='2xl' m={1}/>
+                    </MenuButton>
+                    {/* <MenuList></MenuList> */}
+                </Menu>
+
+                <Menu>
+                    <MenuButton as={Button} rightIcon={<ChevronDownIcon/>}>
+                        <Avatar size='sm' cursor='pointer' name={user.name} src={user.pic}/>
+                    </MenuButton> 
+
+                    <MenuList>
+                        <MenuItem>My Profile</MenuItem>
+                        <MenuItem>Logout</MenuItem>
+                    </MenuList>
+                </Menu>
+            </div>
         </Box>
     </>
   )
