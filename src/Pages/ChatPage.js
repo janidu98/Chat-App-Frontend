8bb29000c61104e450fb05c5c78807beb731c298
janidu98 <janinduchathushan@gmail.com>
2023-11-27 19:46:04 +0530

    modify the chat box in chat page

--- src/Pages/ChatPage.js
+++ src/Pages/ChatPage.js
@@ -3,10 +3,12 @@ import { ChatState } from "../Context/ChatProvider"
 import SideDrawer from "../components/miscellaneous/SideDrawer";
 import MyChats from "../components/MyChats";
 import ChatBox from "../components/ChatBox";
+import { useState } from "react";
  
 const ChatPage = () => {
 
   const {user} = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
 
   return (
     <div style={{ width: "100%" }}>
@@ -15,8 +17,8 @@ const ChatPage = () => {
       <Box
          display="flex" justifyContent="space-between" w='100%' h='91.5vh' p='10px' 
       >
-        {user && <MyChats />}
-        {user && <ChatBox />}
+        {user && <MyChats fetchAgain={fetchAgain}/>}
+        {user && <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>}
       </Box>
     </div>
   )
